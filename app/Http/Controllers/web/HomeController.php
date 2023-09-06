3b872d6676f5e91244f74f3e1baed7fd33bfaece
kimhoang115 <hoangtran@dosiinvn.com>
2023-09-06 15:28:22 +0700

    u

--- app/Http/Controllers/web/HomeController.php
+++ app/Http/Controllers/web/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Country;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('web.home.index');
+        $listCategory = Category::all();
+        $listGenre = Genre::all();
+        $listCountry = Country::all();
+        return view('web.home.index', compact('listCategory', 'listGenre', 'listCountry'));
     }
 
 }
